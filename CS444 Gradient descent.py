169b1469a1b5ac7540a6814248011fe688e44067
DuongTung <duongdoantung2k3@gmail.com>
2022-11-17 15:21:36 +0700

    Update CS444 Gradient descent.py

--- CS444 Gradient descent.py
+++ CS444 Gradient descent.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-_X = np.array([[1, -2, 0], [2, 5, 1]])
+_X = np.array([[1, -2, 0], [2, 5, 1]]).T
 Y = np.array([1,6,1])
-X = np.ones(np.shape(_X.T)[0])
+X = np.ones(np.shape(_X.T)[1])
 X = np.vstack((X, _X.T))
 w = np.array([0,1,5]).T
 def h(w,x):
@@ -10,9 +10,9 @@ def h(w,x):
 def loss(w,x,y):
     return 0.5*np.sum((y.T - h(w,x))**2)
 def grad_descent(w,x,y):
-    return  w + 0.1*(Y.T - h()) @ X
+    return  w + 0.1*(Y.T - h(w,x)) @ X
 for i in range(100):
-    print(f"{i}: {loss()}")
-    w = grad_descent()
+    print(f"{i}: {loss(w,X,Y)}")
+    w = grad_descent(w,X,Y)
 
-print(f"Final: {loss()} {w}")
+print(f"Final: {loss(w,X,Y)} {w}")
